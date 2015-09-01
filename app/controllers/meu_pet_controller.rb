4e9c5dfa9f052779a582b58d6431f1871052a08d
Antonio Reuter <antonio.andrade0@walmart.com>
2015-09-01 13:50:18 -0300

    added the meu_pet controller

---
+++ app/controllers/meu_pet_controller.rb
@@ -0,0 +1,6 @@
+class MeuPetController < ApplicationController
+
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  root 'meu_pet#index'
 
   scope :api do
     resources :reviews, defaults: { format: :json }
