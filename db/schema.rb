45dc41e5bc58308e7c81c3e77efedcbea9e43579
Wellington Pinheiro <wellington.pinheiro@gmail.com>
2015-09-11 00:09:42 -0300

    Add devise settings to app

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150907175200) do
+ActiveRecord::Schema.define(version: 20150911013900) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer  "user_id"
@@ -63,18 +63,29 @@ ActiveRecord::Schema.define(version: 20150907175200) do
   add_index "pets", ["owner_id"], name: "index_pets_on_owner_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",                        null: false
-    t.string   "document",                    null: false
-    t.string   "email",                       null: false
-    t.string   "phone",                       null: false
+    t.string   "name",                                                                 null: false
+    t.string   "document",                                                             null: false
+    t.string   "email",                  default: "---\n:null: false\n:default: ''\n", null: false
+    t.string   "phone",                                                                null: false
     t.text     "description"
-    t.string   "sex",         default: "M"
-    t.boolean  "ong",         default: false
-    t.boolean  "status",      default: true
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.string   "sex",                    default: "M"
+    t.boolean  "ong",                    default: false
+    t.boolean  "status",                 default: true
+    t.datetime "created_at",                                                           null: false
+    t.datetime "updated_at",                                                           null: false
+    t.string   "encrypted_password",     default: "",                                  null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,                                   null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
 
   add_index "users", ["document"], name: "index_users_on_document"
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
