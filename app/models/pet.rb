5e4a7dde6b34b81f3d1681d6d6cf6160209de889
Antonio Reuter <antonio.andrade0@walmart.com>
2015-10-04 23:22:38 -0300

    Fix the relationship problem List the pets

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,8 +2,8 @@ class Pet < ActiveRecord::Base
 
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
   belongs_to :adopted_by, class_name: 'User', foreign_key: 'adopted_by_id'
-  has_one :address
-  has_one :category
+  belongs_to :address
+  belongs_to :category
 
   validates :name, :address, :owner, :category, presence: true
 
